7ffe2c0f04bee25f280b18c2633eff3589a92754
Zach Sherman <zachsherman@cs.uchicago.edu>
2019-05-27 17:29:30 -0500

    opening, closing files works

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ extern crate web_view;
 use std::fs::File;
 use std::path::PathBuf;
 use std::io::prelude::*;
+use std::io::BufReader;
 use web_view::*;
 use crate::Cmd::*;
 
@@ -14,6 +15,7 @@ use crate::Cmd::*;
 pub enum Cmd {
     Save { file: String, content: String },
     Quit {  },
+    Open { },
 }
 
 const INDEX: &str = include_str!("index.html");
@@ -36,9 +38,32 @@ fn main() {
     webview.run().unwrap();
 }
 
-fn handler(_webview: &mut web_view::WebView<'_, ()>, arg: &str )
+fn handler(webview: &mut web_view::WebView<'_, ()>, arg: &str )
     -> WVResult {
     match serde_json::from_str(arg).unwrap() {
+        Open { } => {
+            let path_buf = webview.dialog()
+                                  .open_file("note", "note_title.txt")
+                                  .unwrap();
+            match path_buf {
+                Some(pb) => {
+                    let file_name = pb.to_str().unwrap();
+                    let file = File::open(&pb).unwrap();
+                    let mut buf_reader = BufReader::new(file);
+                    let mut contents = String::new();
+                    match buf_reader.read_to_string(&mut contents) {
+                        Ok(_) => {
+                            webview.eval(&format!("open_file({}, {});",
+                                        web_view::escape(file_name),
+                                        web_view::escape(&contents)))
+                                   .unwrap();
+                            },
+                        Err(_) => webview.eval(&format!("open_error();")).unwrap()
+                    };
+                },
+                None => ()
+            }
+        }
         Save { file, content } => {
             let mut save_file = File::create(&file).unwrap();
             save_file.write_all(content.as_bytes()).unwrap();
